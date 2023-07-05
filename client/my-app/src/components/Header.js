3974f17224248d86673a257dd3dd8eeaa27b22f7
SumariaAK <sumaria.ak@northeastern.edu>
2023-07-04 21:04:15 -0400

    adding name

--- client/my-app/src/components/Header.js
+++ client/my-app/src/components/Header.js
@@ -3,17 +3,20 @@ import './Header.css'
 
 function Header() {
   return (
-    <div>      <header className="header">
-    <h1 className="header-title">Search Application</h1>
-    <div className="header-icons">
-      <a href="https://www.linkedin.com/in/akashsumaria/" target="_blank" rel="noopener noreferrer">
-        <i className="fab fa-linkedin"></i>
-      </a>
-      <a href="https://github.com/SumariaAk" target="_blank" rel="noopener noreferrer">
-        <i className="fab fa-github"></i>
-      </a>
+    <div>      
+      <header className="header">
+        <h1 className="header-title">Search Application</h1>
+        <div className="header-icons">
+          <span className="header-name">Akash Sumaria</span>
+          <a href="https://www.linkedin.com/in/akashsumaria/" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-linkedin"></i>
+          </a>
+          <a href="https://github.com/SumariaAk" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-github"></i>
+          </a>
+        </div>
+      </header>
     </div>
-  </header></div>
   )
 }
 
