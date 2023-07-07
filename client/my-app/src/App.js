e0a3bdeb537fe29aba176947286f5e7b610baa72
SumariaAK <sumaria.ak@northeastern.edu>
2023-07-07 16:11:06 -0400

    updating comments

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -5,19 +5,18 @@ import Search from './components/Search';
 import Header from './components/Header';
 
 function App() {
-  const [topic, setTopic] = useState('');
-  const [results, setResults] = useState([]);
+  const [topic, setTopic] = useState(''); // State for storing the topic entered by the user
+  const [results, setResults] = useState([]); // State for storing the search results
 
   return (
     <div className="App">
-      <Header/ >
+      <Header/ > {/* Render the Header component */}
       <div className="search-container">
-        <Search topic={topic} setTopic={setTopic} setResults={setResults} />
+        <Search topic={topic} setTopic={setTopic} setResults={setResults} /> {/* Render the Search component and pass props */}
       </div>
-      <Result results={results} setResults={setResults} />
+      <Result results={results} setResults={setResults} /> {/* Render the Result component and pass props */}
     </div>
   );
 }
 
 export default App;
-
